a55137f5b346b88e2f29f32214b82a09374fab36
Amy Herbert <ahebert0020@pointloma.edu>
2023-04-19 11:07:34 -0700

    fixed currency stuff

--- Assets/SpawnCurrency.cs
+++ Assets/SpawnCurrency.cs
@@ -8,10 +8,12 @@ public class SpawnCurrency : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     InvokeRepeating("spawn", 5, 5); //the currency will appear every 10 seconds    
+        currency.SetActive(false);
+        InvokeRepeating("spawn", 5, 5); //the currency will appear every 5 seconds    
     }
     void spawn()
     {
+        currency.SetActive(true);
         Vector3 startingPos = new Vector3(0f,2.4f,-3.5f);
         Instantiate(currency, startingPos, Quaternion.identity);
     }
