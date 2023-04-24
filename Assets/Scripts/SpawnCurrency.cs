caa1ed15e9830c1a105db8c5a004862522645a5a
Amy Herbert <ahebert0020@pointloma.edu>
2023-04-24 10:44:11 -0700

    water drops fall from sky

--- Assets/Scripts/SpawnCurrency.cs
+++ Assets/Scripts/SpawnCurrency.cs
@@ -4,7 +4,49 @@ using UnityEngine;
 
 public class SpawnCurrency : MonoBehaviour
 {
-    public GameObject currency;
+    public float min;
+    public float max;
+    public float time;
+    public GameObject waterDrop;
+    public Vector3 minPos;
+    public Vector3 maxPos;
+    
+    Vector3 pos;
+
+    private void Start()
+    {
+        time = Random.Range(min, max);
+        pos.x = Random.Range(minPos.x, maxPos.x);
+        pos.y = Random.Range(minPos.y, maxPos.y);
+        pos.z = Random.Range(minPos.z, maxPos.z);
+        StartCoroutine(spawn());
+    }
+    public IEnumerator spawn()
+    {
+        yield return new WaitForSeconds(time);
+        Instantiate(waterDrop, pos, Quaternion.identity);
+        time = Random.Range(min, max);
+        pos.x = Random.Range(minPos.x, maxPos.x);
+        pos.y = Random.Range(minPos.y, maxPos.y);
+        pos.z = Random.Range(minPos.z, maxPos.z);
+       StartCoroutine(spawn());
+    }
+    void OnCollisionEnter(Collision co)
+    {
+        if(co.gameObject.tag == "WaterDrop")
+        {
+            Destroy(co.gameObject);
+        }
+    }
+    void OnMouseDown() 
+    {
+        // Increase Score
+        //score += 20;
+
+        // Destroy Sun
+        Destroy(waterDrop);
+    }
+    /*public GameObject currency;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +56,12 @@ public class SpawnCurrency : MonoBehaviour
     void spawn()
     {
         currency.SetActive(true);
-        Vector3 startingPos = new Vector3(0f,2.4f,-3.5f);
+        Vector3 startingPos = new Vector3(3f,-3f,-1.5f);
         Instantiate(currency, startingPos, Quaternion.identity);
     }
     // Update is called once per frame
     void Update()
     {
         
-    }
+    }*/
 }
