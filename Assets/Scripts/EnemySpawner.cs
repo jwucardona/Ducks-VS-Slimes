df4f2e9ddbf4b89ef3cb53e8ef643887291cec99
colbytomita <ctomita0020@pointloma.edu>
2023-04-25 19:53:54 -0700

    enemy spawning works

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    public string level;
+    // list of enemies on the level currently
+    List<GameObject> enemies = new List<GameObject>();
+
+    // enemy unit prefabs
+    public GameObject basicSlimePrefab;
+    public GameObject bunnySlimePrefab;
+    public GameObject knightSlimePrefab;
+    public GameObject kingSlimePrefab;
+
+    // list of spawn points
+    [SerializeField] List<GameObject> spawnPoints = new List<GameObject>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        // get random spawn point
+        GameObject spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        GameObject newObject = Instantiate(kingSlimePrefab, new Vector3(spawnPoint.transform.position.x, 1.5f, spawnPoint.transform.position.z), transform.rotation * Quaternion.Euler(0f, 270f, 0f));
     }
 
     // Update is called once per frame
