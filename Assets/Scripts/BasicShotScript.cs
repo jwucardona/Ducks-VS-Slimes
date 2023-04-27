fcbea18d6e90440aa14ff1ddd499b515f4f30322
jwucardona <102259645+jwucardona@users.noreply.github.com>
2023-04-26 21:57:11 -0700

    starting shot collision with enemies (doesnt work)

--- Assets/Scripts/BasicShotScript.cs
+++ Assets/Scripts/BasicShotScript.cs
@@ -7,7 +7,7 @@ public class BasicShotScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,6 +16,20 @@ public class BasicShotScript : MonoBehaviour
         if (transform.position.x > 8)
         {
             Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7 || other.tag == "Enemy")
+        {
+            print("TEST");
+            other.GetComponentInParent<AbstractUnit>().TakeDamage(20);
+            Destroy(this.gameObject);
+        }
+        if (other.tag != "Duck")
+        {
+            print("test");
         }
     }
 }
